017401e8cbeacdd0189fd073f7c401252150ecf4
pdagobert <pdagobert@users.noreply.github.com>
2017-12-11 08:31:54 +0100

    Add first gpu version of erosion

--- Main.cu
+++ Main.cu
@@ -28,13 +28,12 @@ std::vector< float > generateNoiseTexture( int size )
 }
 
 // https://github.com/RolandR/glterrain/blob/master/js/terrain.js
-void erode( std::vector< float >& heightmap, int size )
+void erode( std::vector< float >& heightmap, int size, int iterations )
 {
     const float Scale = 1.0f;
     const float Erosion = 0.0005 * Scale;
     const float Deposition = 0.0000002 * Scale;
     const float Evaporation = 0.9f;
-    const int iterations = 300;
 
     std::vector< float > water( heightmap.size(), 1.0f );
     std::vector< float > dWater( heightmap.size(), 0 );
@@ -47,44 +46,47 @@ void erode( std::vector< float >& heightmap, int size )
         {
             for( int x = 1; x < size - 2; x++ )
             {
-                float tl = heightmap[ x - 1 + ( y + 1 ) * size ];
-                float t = heightmap[ x + ( y + 1 ) * size ];
-                float tr = heightmap[ x + 1 + ( y + 1 ) * size ];
-                float ml = heightmap[ x - 1 + y * size ];
-                float m = heightmap[ x + y * size ];
-                float mr = heightmap[ x + 1 + y * size ];
-                float bl = heightmap[ x - 1 + ( y - 1 ) * size ];
-                float b = heightmap[ x + ( y - 1 ) * size ];
-                float br = heightmap[ x + 1 + ( y - 1 ) * size ];
+                int index = x + y * size;
 
-                float dheight = glm::max( m - tl, 0.0f );
-                dheight += glm::max( m - t, 0.0f );
-                dheight += glm::max( m - tr, 0.0f );
-                dheight += glm::max( m - ml, 0.0f );
-                dheight += glm::max( m - mr, 0.0f );
-                dheight += glm::max( m - bl, 0.0f );
-                dheight += glm::max( m - b, 0.0f );
-                dheight += glm::max( m - br, 0.0f );
+                float m = heightmap[ index ];
+                float dtl = heightmap[ x - 1 + ( y + 1 ) * size ];
+                dtl = glm::max( m - dtl, 0.0f );
+                float dt = heightmap[ x + ( y + 1 ) * size ];
+                dt = glm::max( m - dt, 0.0f );
+                float dtr = heightmap[ x + 1 + ( y + 1 ) * size ];
+                dtr = glm::max( m - dtr, 0.0f );
+                float dml = heightmap[ x - 1 + y * size ];
+                dml = glm::max( m - dml, 0.0f );
+                float dmr = heightmap[ x + 1 + y * size ];
+                dmr = glm::max( m - dmr, 0.0f );
+                float dbl = heightmap[ x - 1 + ( y - 1 ) * size ];
+                dbl = glm::max( m - dbl, 0.0f );
+                float db = heightmap[ x + ( y - 1 ) * size ];
+                db = glm::max( m - db, 0.0f );
+                float dbr = heightmap[ x + 1 + ( y - 1 ) * size ];
+                dbr = glm::max( m - dbr, 0.0f );
 
-                dheightV[ x + y * size ] = dheight;
+                float dheight = dtl + dt + dtr + dml + dmr + dbl + db + dbr;
 
                 if( dheight != 0.0f )
                 {
-                    float w = water[ x + y * size ] * Evaporation;
+                    float w = water[ index ] * Evaporation;
                     float remainingWater = w * 0.0002 / ( dheight * Scale + 1.0f );
                     w -= remainingWater;
 
-                    dWater[ x - 1 + ( y + 1 ) * size ] += ( glm::max( m - tl, 0.0f ) / dheight ) * w;
-                    dWater[ x + ( y + 1 ) * size ] += ( glm::max( m - t, 0.0f ) / dheight ) * w;
-                    dWater[ x + 1 + ( y + 1 ) * size ] += ( glm::max( m - tr, 0.0f ) / dheight ) * w;
-                    dWater[ x - 1 + y * size ] += ( glm::max( m - ml, 0.0f ) / dheight ) * w;
-                    dWater[ x + 1 + y * size ] += ( glm::max( m - mr, 0.0f ) / dheight ) * w;
-                    dWater[ x - 1 + ( y - 1 ) * size ] += ( glm::max( m - bl, 0.0f ) / dheight ) * w;
-                    dWater[ x + ( y - 1 ) * size ] += ( glm::max( m - b, 0.0f ) / dheight ) * w;
-                    dWater[ x + 1 + ( y - 1 ) * size ] += ( glm::max( m - br, 0.0f ) / dheight ) * w;
+                    dWater[ x - 1 + ( y + 1 ) * size ] += dtl / dheight * w;
+                    dWater[ x + ( y + 1 ) * size ] += dt / dheight * w;
+                    dWater[ x + 1 + ( y + 1 ) * size ] += dtr / dheight * w;
+                    dWater[ x - 1 + y * size ] += dml / dheight * w;
+                    dWater[ x + 1 + y * size ] += dmr / dheight * w;
+                    dWater[ x - 1 + ( y - 1 ) * size ] += dbl / dheight * w;
+                    dWater[ x + ( y - 1 ) * size ] += db / dheight * w;
+                    dWater[ x + 1 + ( y - 1 ) * size ] += dbr / dheight * w;
 
-                    water[ x + y * size ] = 1.0f + remainingWater;
+                    water[ index ] = 1.0f + remainingWater;
                 }
+
+                dheightV[ index ] = dheight;
             }
         }
 
@@ -121,12 +123,12 @@ void save( const std::string& fileName, int width, int height, const std::vector
     file.write( reinterpret_cast< const char* >( &heightmap[ 0 ] ), heightmap.size() * sizeof( float ) );
 }
 
-void cpuErosion( int size )
+void cpuErosion( int size, int iterations )
 {
     auto heightmap = generateNoiseTexture( size );
 
     auto start = std::chrono::high_resolution_clock::now();
-    erode( heightmap, size );
+    erode( heightmap, size, iterations );
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast< std::chrono::milliseconds >( end - start ).count();
@@ -136,9 +138,138 @@ void cpuErosion( int size )
     save( "noiseCPU.raw", size, size, heightmap );
 }
 
+__global__
+void erodeKernel( float* heightmap, int size, int iterations, float* water, float* tmpWater, float* dheightV )
+{
+    const float Scale = 1.0f;
+    const float Erosion = 0.0005 * Scale;
+    const float Deposition = 0.0000002 * Scale;
+    const float Evaporation = 0.9f;
+
+    int startx = threadIdx.x + blockIdx.x * blockDim.x;
+    int starty = threadIdx.y + blockIdx.y * blockDim.y;
+    int stridex = blockDim.x * gridDim.x;
+    int stridey = blockDim.y * gridDim.y;
+
+    for( int i = 0; i < iterations; i++ )
+    {
+        for( int y = 1 + starty; y < size - 2; y += stridey )
+        {
+            for( int x = 1 + startx; x < size - 2; x += stridex )
+            {
+                int index = x + y * size;
+
+                float m = heightmap[ index ];
+
+                float dtl = heightmap[ x - 1 + ( y + 1 ) * size ];
+                dtl = glm::max( m - dtl, 0.0f );
+                float dt = heightmap[ x + ( y + 1 ) * size ];
+                dt = glm::max( m - dt, 0.0f );
+                float dtr = heightmap[ x + 1 + ( y + 1 ) * size ];
+                dtr = glm::max( m - dtr, 0.0f );
+                float dml = heightmap[ x - 1 + y * size ];
+                dml = glm::max( m - dml, 0.0f );
+                float dmr = heightmap[ x + 1 + y * size ];
+                dmr = glm::max( m - dmr, 0.0f );
+                float dbl = heightmap[ x - 1 + ( y - 1 ) * size ];
+                dbl = glm::max( m - dbl, 0.0f );
+                float db = heightmap[ x + ( y - 1 ) * size ];
+                db = glm::max( m - db, 0.0f );
+                float dbr = heightmap[ x + 1 + ( y - 1 ) * size ];
+                dbr = glm::max( m - dbr, 0.0f );
+
+                float dheight = dtl + dt + dtr + dml + dmr + dbl + db + dbr;
+
+                if( dheight != 0.0f )
+                {
+                    float w = water[ index ] * Evaporation;
+                    float remainingWater = w * 0.0002 / ( dheight * Scale + 1.0f );
+                    w -= remainingWater;
+
+                    // the only place where race condition can occur
+
+                    atomicAdd( &tmpWater[ x - 1 + ( y + 1 ) * size ], dtl / dheight * w );
+                    atomicAdd( &tmpWater[ x + ( y + 1 ) * size ], dt / dheight * w );
+                    atomicAdd( &tmpWater[ x + 1 + ( y + 1 ) * size ], dtr / dheight * w );
+                    atomicAdd( &tmpWater[ x - 1 + y * size ], dml / dheight * w );
+                    atomicAdd( &tmpWater[ x + 1 + y * size ], dmr / dheight * w );
+                    atomicAdd( &tmpWater[ x - 1 + ( y - 1 ) * size ], dbl / dheight * w );
+                    atomicAdd( &tmpWater[ x + ( y - 1 ) * size ], db / dheight * w );
+                    atomicAdd( &tmpWater[ x + 1 + ( y - 1 ) * size ], dbr / dheight * w );
+
+                    water[ index ] = 1.0f + remainingWater;
+                }
+
+                dheightV[ index ] = dheight;
+            }
+        }
+
+        __syncthreads();
+
+        for( int y = 1 + starty; y < size - 2; y += stridey )
+        {
+            for( int x = 1 + startx; x < size - 2; x += stridex )
+            {
+                int index = x + y * size;
+
+                water[ index ] += tmpWater[ index ];
+                tmpWater[ index ] = 0;
+
+                float oldHeight = heightmap[ index ];
+                heightmap[ index ] += ( -( dheightV[ index ] - 0.005f / Scale ) * water[ index ] ) * Erosion + water[ index ] * Deposition;
+
+                if( oldHeight < heightmap[ index ] )
+                    water[ index ] = glm::max( water[index ] - ( heightmap[ index ] - oldHeight ) * 1000.0f, 0.0f );
+            }
+        }
+
+        __syncthreads();
+    }
+}
+
+void gpuErosion( int size, int iterations )
+{
+    int bufferSize = sizeof( float ) * size * size;
+
+    float* heightmap;
+    cudaMallocManaged( &heightmap, bufferSize );
+
+    float* water;
+    cudaMallocManaged( &water, bufferSize );
+
+    float* tmpWater;
+    cudaMallocManaged( &tmpWater, bufferSize );
+
+    float* dheightV;
+    cudaMallocManaged( &dheightV, bufferSize );
+
+    auto noise = generateNoiseTexture( size );
+
+    for( unsigned int i = 0; i < noise.size(); i++ )
+        heightmap[ i ] = noise[ i ];
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    erodeKernel<<< 4, dim3( 32, 32 ) >>>( heightmap, size, iterations, water, tmpWater, dheightV );
+    cudaDeviceSynchronize();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast< std::chrono::milliseconds >( end - start ).count();
+
+    std::cout << "gpu erosion took " << duration << "ms" << std::endl;
+    save( "noiseGPU.raw", size, size, std::vector< float >( heightmap, heightmap + size * size ) );
+
+    cudaFree( heightmap );
+    cudaFree( water );
+    cudaFree( tmpWater );
+    cudaFree( dheightV );
+}
+
 int main()
 {
-    const int HeightmapSize = 1024;
-    cpuErosion( 1024 );
+    const int HeightmapSize = 2048;
+    const int Iterations = 300;
+    cpuErosion( HeightmapSize, Iterations );
+    gpuErosion( HeightmapSize, Iterations );
     return 0;
 }
